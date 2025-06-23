1f8d0b789e853c6d488f7733394f36e305c317bf
shweta-angularminds <shweta@angularminds.in>
2025-06-23 16:12:45 +0530

    ui: create heading section layout in Home component

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonText,
+  IonButton,
+  IonRow,
+  IonCol,
+  IonSearchbar,
+} from '@ionic/angular/standalone';
+import {
+  location,
+  chevronDownOutline,
+  cart,
+  notifications,
+  options,
+} from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonSearchbar,
+    IonCol,
+    IonRow,
+    IonButton,
+    IonText,
+    IonIcon,
+    IonLabel,
+    IonItem,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ location, chevronDownOutline, cart, notifications, options });
   }
 
+  ngOnInit() {}
 }
